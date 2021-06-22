6bc9bfc7d926a67c35d5c694eb8819fc681bb9cc
Viktoria <doduchenko29@gmail.com>
2021-06-22 20:32:12 +0300

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+const getRandomNumber = (from, to) => 
+  from + Math.random() * (to - from)
+
+const request = url => new Promise(resolve => {
+  const randomDelay = getRandomNumber(1000, 3000);
+  setTimeout(() => {
+    resolve({
+      userData: {
+        name: 'Tom',
+        age: 17,
+      },
+      source: url
+    })
+  }, randomDelay)
+})
+
+const servers = [
+  'https://server.com/us/',
+  'https://server.come/eu/',
+  'https://server.com/au/'
+]
+
+export const getUserASAP = userId => {
+  const userUrls = servers
+    .map(serverUrl => `${serverUrl}${userId}`);
+
+  const requests = userUrls
+    .map(userUrl => request(userUrl))
+
+  return Promise.race(requests)
+}
